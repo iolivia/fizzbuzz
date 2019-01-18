68ea19058a568310ffb33d425a0693232326a1c6
Roger Weiss <roger.d.weiss@gmail.com>
2019-01-18 17:04:17 +0200

    Refactored F# for more functional F#

--- Program.fs
+++ Program.fs
@@ -2,14 +2,11 @@
 
 [<EntryPoint>]
 let main argv =
-    for x in 1..100 do
-        let div3 = (x % 3).Equals(0)
-        let div5 = (x % 5).Equals(0)
-        if (div3 && div5) then 
-            Console.WriteLine("FizzBuzz")
-        else if (div3) then
-            Console.WriteLine("Fizz")
-        else if (div5) then 
-            Console.WriteLine("Buzz")
-        else Console.WriteLine(x.ToString())
+    [1..100] 
+    |> Seq.map (function
+        | x when x%5=0 && x%3=0 -> "FizzBuzz"
+        | x when x%3=0 -> "Fizz"
+        | x when x%5=0 -> "Buzz"
+        | x -> string x)
+    |> Seq.iter (printfn "%s")
     0
