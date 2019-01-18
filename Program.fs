eee0774b9083f1a9d9d47dd02dfc89b8dc57f794
Roger Weiss <roger.d.weiss@gmail.com>
2019-01-18 17:07:59 +0200

    Uses a little more explicit syntax for readability

--- Program.fs
+++ Program.fs
@@ -3,7 +3,8 @@
 [<EntryPoint>]
 let main argv =
     [1..100] 
-    |> Seq.map (function
+    |> Seq.map (fun x ->
+        match x with
         | x when x%5=0 && x%3=0 -> "FizzBuzz"
         | x when x%3=0 -> "Fizz"
         | x when x%5=0 -> "Buzz"
