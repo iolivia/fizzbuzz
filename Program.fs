f0f798e389f9692a7db473462ca2ff887745e59b
Roger Weiss <roger.d.weiss@gmail.com>
2019-01-18 16:57:46 +0200

    Added F#

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    for x in 1..100 do
+        let div3 = (x % 3).Equals(0)
+        let div5 = (x % 5).Equals(0)
+        if (div3 && div5) then 
+            Console.WriteLine("FizzBuzz")
+        else if (div3) then
+            Console.WriteLine("Fizz")
+        else if (div5) then 
+            Console.WriteLine("Buzz")
+        else Console.WriteLine(x.ToString())
+    0
